1538ed85b38947449686cc5c9c0b4a835bb03120
srkok <116056448+srkok@users.noreply.github.com>
2023-08-17 00:11:00 +0900

    Add files via upload

---
+++ chall2_Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float delay = 0.5f;
+    private bool flag = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && flag)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            flag = false;
+            Invoke("ResetFlag", delay);
+        }
+    }
+
+    private void ResetFlag()
+    {
+        flag = true;
+    }
+}
---
+++ chall3_Scripts/PlayerControllerX.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    private float upperBound = 15.0f;
+    private float groundBound = 1.0f;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+    public AudioClip boingSound;
+    private float boingVal = 15.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get playerRb
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+        if (transform.position.y > upperBound)
+        {
+            transform.position = new Vector3(transform.position.x, upperBound, transform.position.z);
+        }
+        if (transform.position.y < groundBound && !gameOver)
+        {
+            transform.position = new Vector3(transform.position.x, groundBound, transform.position.z);
+            playerRb.AddForce(Vector3.up * floatForce * boingVal);
+            playerAudio.PlayOneShot(boingSound, 1.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+}
